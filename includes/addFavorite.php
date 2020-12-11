83aa86cd9745d33941d9fb5282686066c8eb47ef
wnobl374 <wnobl374@mtroyal.ca>
2020-12-10 17:28:21 -0700

    changes to favorites and browse-paintings

--- includes/addFavorite.php
+++ includes/addFavorite.php
@@ -4,4 +4,14 @@
 } else {
     $_SESSION['favorites'] = 
 }*/
-header('Location: favorites.php');
+if (isset($_GET['id']) && $_SESSION['user'] && (($_GET['id']))) {
+    $paintingGate = new PaintingDB($connection);
+    $painting = $paintingGate->getPainting($_GET["id"]);
+    if (!in_array($painting, $_SESSION['favorites'])) {
+        array_push($_SESSION['favorites'], $painting);
+    } else { //if already in array, removes from array
+        unset($$_SESSION['favorites'][$painting['PaintingID']]);
+    }
+} else {
+    header('Location: ../favorites.php');
+}
